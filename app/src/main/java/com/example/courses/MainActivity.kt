042091fec001ef3c4e02c9357a956d5f171f5903
j4n9k4 <janukananayakkara@gmail.com>
2025-02-21 22:31:36 +0530

    Added some more courses Reduced the padding around the topic for my phone

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -4,14 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.courses.data.DataSource
+import com.example.courses.model.Topic
 import com.example.courses.ui.theme.CoursesTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +41,100 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoursesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CoursesApp()
+                }
 
             }
         }
     }
 }
+@Composable
+fun CoursesApp()
+{
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateEndPadding(layoutDirection)
+            )
+    ) {
+        TopicGrid(DataSource.topics)
+    }
+}
 
+@Composable
+fun TopicGrid(
+    listOfTopics: List<Topic>,
+    modifier: Modifier = Modifier
+)
+{
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+    ) {
+        items(listOfTopics){ topic ->
+            Topic(topic, Modifier.padding(7.dp))
+        }
+    }
+}
+@Composable
+fun Topic(
+    topic: Topic,
+    modifier: Modifier = Modifier,
+
+)
+{
+    Card(modifier = modifier) {
+        Row {
+            Image(
+                painter = painterResource(topic.coverImage),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(68.dp)
+                    .height(68.dp)
+            )
+
+            Column {
+                Text(
+                    text = stringResource(topic.topic),
+                    modifier = Modifier
+                            .padding(top = 16.dp,
+                                bottom = 8.dp,
+                                start = 16.dp,
+                                end = 12.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Row {
+                    Image(
+                        painter = painterResource(R.drawable.courses_icon),
+                        contentDescription = stringResource(R.string.number_of_courses),
+                        modifier = Modifier
+                            .padding(start = 16.dp, end = 8.dp)
+                            .width(20.dp)
+                            .height(20.dp)
+                    )
+                    Text(
+                        text = topic.courseCount.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CoursesAppPreview()
+{
+    TopicGrid(DataSource.topics)
+}
